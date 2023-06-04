fefb7a76ddc0b595d45ccdedc80f7082fbd69d5a
VitaliyDunyushkin <117518206+VitaliyDunyushkin@users.noreply.github.com>
2023-06-04 18:33:48 +0300

    done step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Feedback from './Feedback/Feedback';
+
+export class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+
+  doGood = event => {
+    const btnText = event.target.textContent.toLowerCase();
+    this.setState(prevState => ({ good: prevState.good + 1 }));
+  };
+
+  doNeutral = event => {
+    const btnText = event.target.textContent.toLowerCase();
+    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
+  };
+
+  doBad = event => {
+    const btnText = event.target.textContent.toLowerCase();
+    this.setState(prevState => ({ bad: prevState.bad + 1 }));
+  };
+
+  render() {
+    return (
+      <>
+        <Feedback
+          doGood={this.doGood}
+          doNeutral={this.doNeutral}
+          doBad={this.doBad}
+          stats={this.state}
+        />
+      </>
+    );
+  }
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,41 @@
+import css from './feedback.module.css';
+
+export default function Feedback({ doGood, doNeutral, doBad, stats }) {
+  const { good, neutral, bad } = stats;
+  const total = good + neutral + bad;
+  const percentOfGood = total === 0 ? 0 : Math.round((good / total) * 100);
+
+  return (
+    <>
+      <p className={css.feedbackTitle}>Please leave feedback</p>
+      <button type="button" className={css.feedbackButton} onClick={doGood}>
+        Good
+      </button>
+      <button type="button" className={css.feedbackButton} onClick={doNeutral}>
+        Neutral
+      </button>
+      <button type="button" className={css.feedbackButton} onClick={doBad}>
+        Bad
+      </button>
+      <p className={css.feedbackTitle}>Statistics</p>
+      <ul>
+        <li className={css.statsItem}>
+          Good: <span className={css.statsNumber}>{good}</span>
+        </li>
+        <li className={css.statsItem}>
+          Neutral: <span className={css.statsNumber}>{neutral}</span>
+        </li>
+        <li className={css.statsItem}>
+          Bad: <span className={css.statsNumber}>{bad}</span>
+        </li>
+        <li className={css.statsItem}>
+          Total: <span className={css.statsNumber}>{total}</span>
+        </li>
+        <li className={css.statsItem}>
+          Positive feedback:{' '}
+          <span className={css.statsNumber}>{percentOfGood}%</span>
+        </li>
+      </ul>
+    </>
+  );
+}
